bec0c6bfe08207a4cdc0cb09ef29a85f7bb68772
Vishwak Naidu <pullepuviswak08@gmail.com>
2026-05-02 21:32:44 +0530

    fix: add vite config and update production paths

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+  server: {
+    port: 5501,
+  }
+})
